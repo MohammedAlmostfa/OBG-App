abdf962e07a796715232106b36dd9ea0d76cc29e
Mohammed Hussien Almostfa <146775849+MohammedAlmostfa@users.noreply.github.com>
2026-04-12 21:44:26 +0300

    update in forget password

--- app/Mail/VerifyEmail.php
+++ app/Mail/VerifyEmail.php
@@ -3,10 +3,10 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
+
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
-use App\Models\User;
+
 
 class VerifyEmail extends Mailable
 {
--- app/Services/ForgetPassword/ForgetPasswordRequestService.php
+++ app/Services/ForgetPassword/ForgetPasswordRequestService.php
@@ -15,7 +15,6 @@ class ForgetPasswordRequestService
     {
         return  [
             'password',
-            'chat id',
             'email',
             'code',
         ];
